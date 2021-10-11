542d2fbe25385a9c2a67ee07f264f86596ee51b5
ferdhian surya <ferdhian2416@student.uns.ac.id>
2021-10-11 14:54:36 +0700

    Create 5.py

---
+++ chapter4/5.py
@@ -0,0 +1,4 @@
+jmlhLaki_Laki = int(input('total mahasiswa laki laki: '))
+jmlhPerempuan = int(input('total mahasiswa perempuan: '))
+print('\nlaki-laki :', 'x' * (jmlhLaki_Laki // 6), jmlhLaki_Laki)
+print('perempuan :', 'x' * (jmlhPerempuan // 6), jmlhPerempuan)
