0b2d9301e33f2badabff9628b228a358adec7f07
ferdhian surya <ferdhian2416@student.uns.ac.id>
2021-10-20 10:54:03 +0700

    Create lulus 3.py

---
+++ chapter 5/lulus 3.py
@@ -0,0 +1,26 @@
+minim_nilai = 60
+nilai_mtk = 71
+batas_bwh = 0
+batas_ats = 100
+
+#input data
+print('STATUS KELULUSAN UJIAN MAHASISWA')
+
+print(input('NAMA MAHASISWA: '))
+
+nilai_bi = (int(input('NILAI BAHASA INDONESIA: ')))
+nilai_ipa = (int(input('NILAI ILMU PENGETAHUAN ALAM: ')))
+nilai_mat = (int(input('NILAI MATEMATIKA: ')))
+if(batas_bwh > nilai_bi) or (batas_ats < nilai_bi) or (batas_bwh > nilai_mat) or (batas_ats < nilai_mat) or (batas_bwh > nilai_ipa) or (batas_ats < nilai_ipa):
+    print('            NILAI TIDAK VALID         ')
+elif(minim_nilai < nilai_bi) and (minim_nilai < nilai_ipa) and (minim_nilai < nilai_mat) and (nilai_mtk < nilai_mat):
+    print('        LULUS        ')
+else:
+    print('         TIDAK LULUS     ')
+    if(nilai_bi < minim_nilai):
+        print('- NILAI BAHASA INDONESIA KURANG DARI 60')
+    if(nilai_ipa < minim_nilai):
+        print('- NILAI ILMU PENGETAHUAN ALAM KURANG DARI 60')
+    if(nilai_mat < minim_nilai) and (nilai_mat < nilai_mtk):
+        print('- NILAI MATEMATIKA KURANG DARI 71')
+
