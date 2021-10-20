7321050a127d71722dcc9bb12d5d22ad61a9a77a
ferdhian surya <ferdhian2416@student.uns.ac.id>
2021-10-20 10:52:46 +0700

    Create ferdhian_praktikum 1.py

---
+++ chapter 5/ferdhian_praktikum 1.py
@@ -0,0 +1,14 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> type(10)
+<class 'int'>
+>>> type(10.1918)
+<class 'float'>
+>>> type('hello world')
+<class 'str'>
+>>> type(true)
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    type(true)
+NameError: name 'true' is not defined
+>>> 
