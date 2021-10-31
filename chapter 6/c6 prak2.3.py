3c7698372b3e95bc433329c972b47110e48091fe
ferdhian surya <ferdhian2416@student.uns.ac.id>
2021-10-31 20:19:20 +0700

    Create c6 prak2.3.py

---
+++ chapter 6/c6 prak2.3.py
@@ -0,0 +1,13 @@
+def luasSegitiga(alas, tinggi):
+    luas = alas * tinggi / 2
+    return luas
+alas = 10
+tinggi = 20
+print('luas segitiga dg alas', alas, 'dan tinggi', tinggi,'adalah', luasSegitiga(alas, tinggi))
+
+def luasSegitiga2(alas, tinggi):
+    luas = alas * tinggi / 2
+    return luas
+alas = 15
+tinggi = 45
+print('luas segitiga2 dg alas', alas, 'dan tinggi', tinggi,'adalah', luasSegitiga2(alas, tinggi))
