6ed12494eab5e7ab8d059c3da1c070270ebf4316
ferdhian surya <ferdhian2416@student.uns.ac.id>
2021-10-31 20:19:31 +0700

    Create operasi.py

---
+++ chapter 6/operasi.py
@@ -0,0 +1,25 @@
+from operation import *
+
+a = kali(4,6)
+b = jumlah(2,a)
+c = kurang(b,4)
+
+print('a. 2 + 4 * 6 - 4 =', c)
+
+a = jumlah(4,7)
+b = kurang(6,9)
+c = kali(a,b)
+
+print('b. (4 + 7) * (6 - 9) =', c)
+
+a = jumlah(10,2)
+b = jumlah (7,5)
+c = kurang(12,34)
+d = bagi(a,b)
+e = bagi(d,c)
+
+print('c. (10 + 2) / (7 + 5) / (12 - 34) =', e)
+
+
+
+
