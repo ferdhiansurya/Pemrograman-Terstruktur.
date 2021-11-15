aaf232173c376d0b4347a4763af4410a6dd302ef
ferdhian surya <ferdhian2416@student.uns.ac.id>
2021-11-15 21:38:10 +0700

    Create c8 lat6.py

---
+++ chapter 8/c8 lat6.py
@@ -0,0 +1,11 @@
+def sortStringByChar(a):
+    huruf = []
+    for i in a:
+        proses = len(i)
+        huruf. append(proses)
+        a. sort(reverse=True)
+    return a
+
+misal_data = ['apel', 'rambutan', 'jeruk']
+print(misal_data)
+print(sortStringByChar(misal_data))
