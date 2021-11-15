f503d96b88755bbb5597f8b8a385bc1154c6e90e
ferdhian surya <ferdhian2416@student.uns.ac.id>
2021-11-15 21:38:06 +0700

    Create c8 lat7.py

---
+++ chapter 8/c8 lat7.py
@@ -0,0 +1,7 @@
+hargaBuah = {'apel' : 5000, 'jeruk' : 8500, 'mangga' : 7800, 'duku' : 6800}
+def termahal(hargaBuah):
+    maks = max(hargaBuah. values())
+    return maks
+
+print('harga buah termahal adalah buah jeruk dengan', termahal(hargaBuah))
+
