e6a87442c7ea445c7b74f9ae8faf8561d07952ff
ferdhian surya <ferdhian2416@student.uns.ac.id>
2021-11-15 21:37:50 +0700

    Create c8 lat11.py

---
+++ chapter 8/c8 lat11.py
@@ -0,0 +1,63 @@
+hargaBuah = {'apel' : 5000, 'jeruk' : 8500, 'mangga' : 7800, 'duku' : 6800}
+while True:
+    print('='*24)
+    print('Silakan pilih menu dibawah')
+    print('='*24)
+    print('A. Tambah data buah')
+    print('B. Beli buah')
+    print('C. Cukup')
+    a = input('Pilihan anda : ')
+    if a in ('A', 'a'):
+        tambah = input('Masukkan nama buah : ')
+        if tambah in hargaBuah :
+            print('Buah sudah ada')
+            True
+        else :
+            perKilo = int(input('Harga satu Kilo : '))
+            hargaBuah[tambah] = perKilo
+            print('Data buah :')
+            for i,j in hargaBuah. items():
+                print('-',i,'(Harga Rp.'+str(j)+')')    
+            print('')
+            True
+
+    elif a in ('B', 'b'):
+        print('='*24)
+        print('TOTAL PEMBAYARAN BUAH')
+        print('='*24)
+        print(hargaBuah)
+        hasil = 0
+        try :
+            while True :
+                print('')
+                buah = input('Buah yang dibeli : ')
+                if buah in hargaBuah :
+                    harga = int(input('Banyaknya buah (KG) : '))
+                    lagi = input('Beli buah lain? (y/n) : ')
+                    if lagi in ('Y', 'y'):
+                        True
+                    elif lagi in ('N', 'n'):
+                        print('-'*24)
+                        total = harga * hargaBuah[buah]
+                        hasil += total
+                        print('total nominal : Rp.',hasil)
+                        break
+                    else :
+                        print('Maaf input tidak valid')
+                        break
+                else:
+                    print('='*24)
+                    print('Maaf input tidak valid')
+                    break
+        except ValueError :
+            print('Maaf harus angka ya!')
+    elif a in ('C', 'c'):
+        break
+            
+    else :
+        print('Mohon isi dengan benar')
+        break
+
+
+
+
