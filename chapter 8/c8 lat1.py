852699087afd3ce0fe0a4ffbece10095eac60786
ferdhian surya <ferdhian2416@student.uns.ac.id>
2021-11-15 21:38:24 +0700

    Create c8 lat1.py

---
+++ chapter 8/c8 lat1.py
@@ -0,0 +1,10 @@
+print('='*24)
+print('BUKAN SULAP BUKAN SIHIR')
+print('='*24)
+bil = []
+banyak = int(input('kamu mau memasukkan berapa bilangan? : '))
+for i in range (banyak):
+    bulat = int(input('silakan masukkan angkamu : '))
+    bil. append(bulat)
+bil. sort(reverse = True)
+print(bil)
