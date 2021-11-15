067422e3117adc31f57cf74f52963133871c9738
ferdhian surya <ferdhian2416@student.uns.ac.id>
2021-11-15 21:38:18 +0700

    Create c8 lat3.py

---
+++ chapter 8/c8 lat3.py
@@ -0,0 +1,7 @@
+banyak = []
+mahasiswa = int(input('kamu mau memasukkan berapa nama mahasiswa sih? : '))
+for i in range(mahasiswa):
+    nama = input('nama mahasiswa : ')
+    banyak. append(nama)
+for nama in banyak :
+    print(nama,'(',str(len(nama)), 'karakter)')
