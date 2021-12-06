952009e963ac561d445d2375ca854670b5de4d28
ferdhian surya <ferdhian2416@student.uns.ac.id>
2021-12-06 11:00:05 +0700

    Create cptr10_create1.py

---
+++ chapter 10/cptr10_create1.py
@@ -0,0 +1,12 @@
+create = open('D:\kodingku01.txt', 'a')
+
+create.write('100\n')
+create.write('102\n')
+create.write('99\n')
+create.write('89\n')
+create.write('192\n')
+create.write('938\n')
+create.write('107\n')
+create.write('241\n')
+
+create.close()
